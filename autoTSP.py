eea441e523b7f0de55de2eefd0117df7b0130f45
lorpyzz <jvlopes.costa14@gmail.com>
2026-03-05 01:34:13 -0300

    Update autoTSP.py

--- autoTSP.py
+++ autoTSP.py
@@ -1,3 +1,5 @@
+import math
+
 def ler_arqTSP(arq):
     arquivo = open(arq, 'r')
     linhas = arquivo.readlines()
@@ -16,6 +18,19 @@ def extrair_coordenadas(linhas):
 
     return lista_pontos
 
+def calcular_distancia(x1,y1,x2,y2):
+    delta_x = x2 - x1
+    delta_y = y2 - y1
+
+    distancia = math.sqrt(delta_x**2 + delta_y**2)
+    #fazendo pitágoras para achar uma distancia (hipotenusa) entre dois pontos
+
+    return distancia
+
+#teste para distancia
 linhas = ler_arqTSP("rat99.tsp")
 pontos = extrair_coordenadas(linhas)
-print(pontos)
+
+distancia = calcular_distancia(int(pontos[0][1]),int(pontos[0][2]),int(pontos[1][1]),int(pontos[1][2]))
+print(distancia)
+#resultado = 14.212670, verificado e certa resposta
