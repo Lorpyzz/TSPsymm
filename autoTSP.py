405b8a719c5919bac9f492c1a315ccd11704c83a
lorpyzz <jvlopes.costa14@gmail.com>
2026-03-05 01:19:52 -0300

    Update autoTSP.py

--- autoTSP.py
+++ autoTSP.py
@@ -4,7 +4,18 @@ def ler_arqTSP(arq):
     arquivo.close()
     return linhas
 
-arquivo = ler_arqTSP("rat99.tsp")
-print(arquivo)
+def extrair_coordenadas(linhas):
+    lista_pontos = []
 
-#botamos esse arquivo dentro do programa, transformando nele em uma enorme lista com toda informação
+    for i in range(6, len(linhas)-1):
+        #as coordenadas começam da linha 7
+        #tirando tambem a ultima linha "EOF"
+        linha_atual = linhas[i].strip()
+        partes = linha_atual.split()
+        lista_pontos.append(partes)
+
+    return lista_pontos
+
+linhas = ler_arqTSP("rat99.tsp")
+pontos = extrair_coordenadas(linhas)
+print(pontos)
